57e7f29a59663161d5932167febc67e0de0f0c68
Denis Chornyi <chornyi.den@gmail.com>
2024-06-06 10:52:01 +0300

    fix

---
+++ task1/calculator/index.js
@@ -0,0 +1,13 @@
+const sum = (a, b) => {
+  console.log("implementation for sum");
+};
+
+const mult = (a, b) => {
+  console.log("implementation for mult");
+};
+
+const calc = (expression) => {
+  console.log("implementation for calc");
+};
+
+export default calc
---
+++ task1/index.js
@@ -0,0 +1,11 @@
+import calc from "./calculator/index.js";
+
+const calcResult = calcExpression('1 + 2');
+
+const sumResult = sum(1, 2);
+
+const multResult = mult(1, 2);
+
+const userDataPromise = fetchUser('facebook');
+
+printProfile({ name: 'Tom', from: 'The world' });
