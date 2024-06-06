33709e7df0032af8e6c4d23c35ea656c5e0ca90d
Denis Chornyi <chornyi.den@gmail.com>
2024-06-06 13:25:59 +0300

    f

--- polyfills/array-flat.js
+++
@@ -1,7 +0,0 @@
-console.log('polyfill for flat');
-
-window.Array.prototype.flat =
-  Array.prototype.flat ||
-  function flat() {
-    // ... implementation for older browsers
-  };
--- polyfills/array-flatMap.js
+++
@@ -1,7 +0,0 @@
-console.log('polyfill for flatMap');
-
-window.Array.prototype.flatMap =
-  Array.prototype.flatMap ||
-  function flatMap() {
-    // ... implementation for older browsers
-  };
