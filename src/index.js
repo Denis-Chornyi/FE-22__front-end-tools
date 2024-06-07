aff608b61cdac63d05f25a7717adb4a33fdcd560
Denis Chornyi <chornyi.den@gmail.com>
2024-06-07 20:56:55 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import { printProfile } from './profile.js';
+import { initTodoListHandlers } from './list/todoList.js';
+import { renderTasks } from './list/renderer.js';
+import { getTasksList } from './list/tasksGateway.js';
+import { setItem } from './list/storage.js';
+import './index.scss'
 
-const userData = {
-  name: 'Tom',
-  age: 17,
+document.addEventListener('DOMContentLoaded', () => {
+  getTasksList().then(tasksList => {
+    setItem('tasksList', tasksList);
+    renderTasks();
+  });
+  initTodoListHandlers();
+});
+
+const onStorageChange = e => {
+  if (e.key === 'tasksList') {
+    renderTasks();
+  }
 };
 
-const profile = {
-  ...userData,
-  company: 'Gromcode',
-};
-
-printProfile(profile);
-
-const data = [1, 2, [3, 4, [5, 6]]];
-
-const result = data.flat(2);
-console.log(result);
-
-Promise.resolve().finally();
+window.addEventListener('storage', onStorageChange);
---
+++ src/list/deleteData.js
@@ -0,0 +1,12 @@
+import { renderTasks } from './renderer.js';
+import { setItem } from './storage.js';
+import { deleteTask, getTasksList } from './tasksGateway.js';
+
+export const onDeleteTask = e => {
+  const taskId = e.target.closest('.list__item').dataset.id;
+  console.log(taskId);
+  deleteTask(taskId)
+    .then(() => getTasksList())
+    .then(updatedTasksList => setItem('tasksList', updatedTasksList));
+  renderTasks();
+};
