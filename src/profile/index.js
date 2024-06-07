0dd158dc93c3ecdcd7d6adc185f9ba6cb3ded75a
Denis Chornyi <chornyi.den@gmail.com>
2024-06-07 20:52:16 +0300

    fix

--- src/profile/index.js
+++
@@ -1,5 +0,0 @@
-export const printProfile = profileData => {
-  console.log('implementation for printProfile');
-  const { name, company } = profileData;
-  console.log(`${name} from ${company}`);
-};
