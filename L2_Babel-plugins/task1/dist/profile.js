b58a9cfd02f801548663cb25d87e26e2c9be12ba
Denis Chornyi <chornyi.den@gmail.com>
2024-06-11 13:59:42 +0300

    lessons

---
+++ L2_Babel-plugins/task1/dist/profile.js
@@ -0,0 +1,4 @@
+const printProfile = function (profileData) {
+  const { name, company } = profileData;
+  console.log(`${name} from ${company}`);
+};
---
+++ L9_Code-formatting_ESLint/task1/src/index.js
@@ -0,0 +1,14 @@
+import printProfile from './renderProfile.js';
+
+const userData = {
+  name: 'Tom',
+  age: 17
+};
+
+const profile = {
+  ...userData,
+  company: 'Gromcode'
+};
+
+printProfile(profile);
+
